2caece796ee957c35b90b5f8735439b9d4030232
zshosha <zshosha424@gmail.com>
2026-07-10 15:19:51 +0300

    fix(images): whitelist external domains in next config for image optimization

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,24 @@ const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@prisma/client", "bcrypt", "bcryptjs"],
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "res.cloudinary.com" }],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
   },
 };
 
