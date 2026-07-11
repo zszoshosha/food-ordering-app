d737b4f6c3aef58a5d42363b6597407330d779fe
zshosha <zshosha424@gmail.com>
2026-07-12 02:37:01 +0300

    fix(images): whitelist res.cloudinary.com for image optimization

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,7 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
+        pathname: "/**",
       },
       {
         protocol: "https",
